55a3956ed15c98c20d3355208e315e4b745151cf
hamzakataf <hmzakataf@gmail.com>
2025-04-28 20:44:21 -0400

    add scope folder and file html and file js them talk about varibles

---
+++ Scope/scope_lab.js
@@ -0,0 +1,12 @@
+// النطاق العالمي
+var globalVar = "أنا متغير عالمي";
+let globalLet = "أنا أيضاً عالمي، لكن محدد باستخدام let";
+const globalConst = "أنا ثابت عالمي";
+
+
+{
+// نطاق الكتلة
+var blockVar = "أنا var محدود بالنطاق";
+let blockLet = "أنا let محدود بالنطاق";
+const blockConst = "أنا const محدود بالنطاق";
+}
